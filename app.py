5a99affe12a88ff0965c64d968a513c467c34fe4
borreti <36523422+borreti@users.noreply.github.com>
2021-07-25 20:59:22 -0300

    adding cors and jwt secreet

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
 class App:
     app = Flask(__name__)
     
     @staticmethod
     def start_server():
-        App.app.run()
+        App.app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
         CORS(App.app)
-    
+        App.app.run()
