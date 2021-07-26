eee4ab8850b51c35815ebfd6eb28f8ba799fa5d2
borreti <36523422+borreti@users.noreply.github.com>
2021-07-26 00:29:25 -0300

    adding adding track modifications flag

--- orm_database.py
+++ orm_database.py
@@ -3,6 +3,7 @@ from app import App
 import os
 
 App.app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+App.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 class ORMDatabase():
     db = SQLAlchemy(App.app)
