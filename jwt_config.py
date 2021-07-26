0cc2e25ac3d5801d6d6b7001820ad458dab45221
borreti <36523422+borreti@users.noreply.github.com>
2021-07-26 00:28:44 -0300

    change '

--- jwt_config.py
+++ jwt_config.py
@@ -20,5 +20,5 @@ class JWTConfig:
     @staticmethod
     @jwt.user_lookup_loader
     def user_lookup_callback(_jwt_header, jwt_data):
-        identity = jwt_data["sub"]
+        identity = jwt_data['sub']
         return User.query.filter_by(id=identity).one_or_none()
