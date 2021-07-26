3ee6dc0adc2db8afc965c03c6814d03c1f571f27
borreti <36523422+borreti@users.noreply.github.com>
2021-07-25 21:01:19 -0300

    rename and add method to check the password

--- src/api/models/user.py
+++
@@ -1,7 +0,0 @@
-from orm_database import ORMDatabase
-
-class User(ORMDatabase.db.Model):
-    id = ORMDatabase.db.Column(ORMDatabase.db.Integer, primary_key=True)
-    username = ORMDatabase.db.Column(ORMDatabase.db.String(80), unique=True, nullable=False)
-    email = ORMDatabase.db.Column(ORMDatabase.db.String(120), unique=True, nullable=False)
-    password = ORMDatabase.db.Column(ORMDatabase.db.String(100))
---
+++ src/api/models/users.py
@@ -0,0 +1,31 @@
+from orm_database import ORMDatabase
+import datetime
+from flask_bcrypt import Bcrypt
+from app import App
+import jwt
+import os
+
+class User(ORMDatabase.db.Model):
+    
+    __tablename__= 'users'
+    
+    __bcrypt = Bcrypt(App.app)
+    
+    id = ORMDatabase.db.Column(ORMDatabase.db.Integer, primary_key=True)
+    username = ORMDatabase.db.Column(ORMDatabase.db.String(80), unique=True, nullable=False)
+    # email = ORMDatabase.db.Column(ORMDatabase.db.String(120), unique=True, nullable=False)
+    password = ORMDatabase.db.Column(ORMDatabase.db.String(100), nullable=False)
+    created_at = ORMDatabase.db.Column(ORMDatabase.db.DateTime, nullable=False)
+    
+    def __init__(self, username = None, password = None):
+        self.username = username
+        self.password = User.__bcrypt.generate_password_hash(
+            password
+        ).decode()
+        self.created_at = datetime.datetime.now()
+        
+    def check_password(self, password):
+        if User.__bcrypt.check_password_hash(self.password, password):
+            return True
+        else:
+            return False
