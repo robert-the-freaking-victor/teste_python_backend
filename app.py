f5cfe5b4aa7cc7d1925bbb73086723c475edc075
borreti <36523422+borreti@users.noreply.github.com>
2021-07-27 00:03:10 -0300

    adding token expires on start_server

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+from datetime import timedelta
 
 class App:
     app = Flask(__name__)
@@ -8,5 +9,7 @@ class App:
     @staticmethod
     def start_server():
         App.app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET')
+        token_expires = os.getenv('JWT_ACCESS_TOKEN_EXPIRES')
+        App.app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(token_expires)
         CORS(App.app)
         App.app.run()
